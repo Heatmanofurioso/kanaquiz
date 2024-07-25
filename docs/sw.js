703b7106b504966ca49a8705ed2c2010880f7058
Heatmanofurioso <casinhas32@hotmail.com>
2024-07-25 10:50:18 +0100

    Remove unused docs

--- docs/sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("precache-manifest.326c036b28e7454302ef7a38b020e21b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-
